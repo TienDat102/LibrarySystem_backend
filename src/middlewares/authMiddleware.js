8ed5589cd10e6c3a0f76d1d233c906639ca7168c
TienDat102 <146631701+TienDat102@users.noreply.github.com>
2024-09-22 00:41:16 +0700

    2Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,19 +1,40 @@
+// const jwt = require('jsonwebtoken');
+
+// module.exports = function (req, res, next) {
+//     const token = req.headers.authorization;
+//     console.log("Token:", token)
+//     if (token) {
+//         const access = token.split(" ")[1];
+//         jwt.verify(access, 'DungLapLanh', (err, decodedToken) => {
+//             if (err) {
+//                 console.log(err.message)
+//                 return res.status(403).json({ message: 'Token không hợp lệ' });
+//             }
+//             else {
+//                 req.user = decodedToken;
+//                 next()
+//             }
+//         })
+//     } else res.status(401).json({ message: 'Authentication required' });
+// };
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
     const token = req.headers.authorization;
-    console.log("Token:", token)
+    console.log("Token:", token);
+
     if (token) {
         const access = token.split(" ")[1];
-        jwt.verify(access, 'DungLapLanh', (err, decodedToken) => {
+        jwt.verify(access, process.env.JWT_SECRET, (err, decodedToken) => {
             if (err) {
-                console.log(err.message)
+                console.log(err.message);
                 return res.status(403).json({ message: 'Token không hợp lệ' });
+            } else {
+                req.user = decodedToken; // Lưu thông tin người dùng đã giải mã
+                next(); // Tiếp tục với middleware tiếp theo
             }
-            else {
-                req.user = decodedToken;
-                next()
-            }
-        })
-    } else res.status(401).json({ message: 'Authentication required' });
+        });
+    } else {
+        res.status(401).json({ message: 'Authentication required' });
+    }
 };

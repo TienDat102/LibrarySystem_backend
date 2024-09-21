d27045b3bfc8f580933ff9c6e7d0f9da05b00465
TienDat102 <146631701+TienDat102@users.noreply.github.com>
2024-09-22 01:01:00 +0700

    làn cuối nhé Update auth.js

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,19 +1,74 @@
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-const { asyncHandler } = require('../middlewares/asyncHandler')
-const cookie = require('cookie')
-const maxAge = 60 * 60
+// const User = require('../models/User')
+// const jwt = require('jsonwebtoken')
+// const { asyncHandler } = require('../middlewares/asyncHandler')
+// const cookie = require('cookie')
+// const maxAge = 60 * 60
+// const createToken = (id) => {
+//     return jwt.sign({ userID: id }, 'DungLapLanh', {
+//         expiresIn: '1h'
+//     })
+// }
+// const parse = (jwt) => {
+//     const setCookie = jwt
+//     const cookies = cookie.parse(setCookie)
+//     const jwtToken = cookies.jwt
+//     return jwtToken
+// }
+// const registerUser = asyncHandler(async (req, res) => {
+//     const { username, msv, email, password } = req.body;
+
+//     if (!username || !msv || !email || !password) {
+//         throw new Error("Please fill all the inputs.");
+//     }
+
+//     const userExists = await User.findOne({ msv });
+//     if (userExists) { return res.status(400).send({ message: "User already exists", success: false }) };
+
+//     const newUser = new User({ studentName: username, msv: msv, email: email, password: password });
+
+//     try {
+//         await newUser.save();
+//         return res.status(201).json({ message: "Create account success", newUser: newUser._id, success: true });
+//     } catch (error) {
+//         console.error("Error saving user:", error);
+//         return res.status(400).send({ message: "Invalid user data", success: false });
+//     }
+// });
+// const loginUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+//     try {
+//         const user = await User.login(email, password);
+//         const token = createToken(user._id);
+//         res.cookie('jwt', token, { 
+//             httpOnly: false, 
+//             secure: process.env.NODE_ENV === 'production', 
+//             path: "/" 
+//         });
+//         return res.status(200).json({
+//             message: "Đăng nhập thành công",
+//             success: true,
+//             user: user._id.toString(),
+//             token
+//         });
+//     } catch (error) {
+//         return res.status(400).json({ message: "Đăng nhập thất bại", success: false, error });
+//     }
+// });
+
+// const logoutUser = async (req, res) => {
+//     res.clearCookie('jwt', { path: '/' });
+//     res.status(200).json({ message: 'Đăng xuất thành công!' });
+// }
+
+// module.exports = { registerUser, loginUser, logoutUser }
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { asyncHandler } = require('../middlewares/asyncHandler');
+
 const createToken = (id) => {
-    return jwt.sign({ userID: id }, 'DungLapLanh', {
-        expiresIn: '1h'
-    })
-}
-const parse = (jwt) => {
-    const setCookie = jwt
-    const cookies = cookie.parse(setCookie)
-    const jwtToken = cookies.jwt
-    return jwtToken
-}
+    return jwt.sign({ userID: id }, 'DungLapLanh', { expiresIn: '1h' });
+};
+
 const registerUser = asyncHandler(async (req, res) => {
     const { username, msv, email, password } = req.body;
 
@@ -22,9 +77,11 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const userExists = await User.findOne({ msv });
-    if (userExists) { return res.status(400).send({ message: "User already exists", success: false }) };
+    if (userExists) {
+        return res.status(400).send({ message: "User already exists", success: false });
+    }
 
-    const newUser = new User({ studentName: username, msv: msv, email: email, password: password });
+    const newUser = new User({ studentName: username, msv, email, password });
 
     try {
         await newUser.save();
@@ -34,21 +91,17 @@ const registerUser = asyncHandler(async (req, res) => {
         return res.status(400).send({ message: "Invalid user data", success: false });
     }
 });
+
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.login(email, password);
         const token = createToken(user._id);
-        res.cookie('jwt', token, { 
-            httpOnly: false, 
-            secure: process.env.NODE_ENV === 'production', 
-            path: "/" 
-        });
         return res.status(200).json({
             message: "Đăng nhập thành công",
             success: true,
             user: user._id.toString(),
-            token
+            token, // Gửi token về frontend
         });
     } catch (error) {
         return res.status(400).json({ message: "Đăng nhập thất bại", success: false, error });
@@ -56,8 +109,8 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = async (req, res) => {
-    res.clearCookie('jwt', { path: '/' });
     res.status(200).json({ message: 'Đăng xuất thành công!' });
-}
+};
 
-module.exports = { registerUser, loginUser, logoutUser }
+module.exports = { registerUser, loginUser, logoutUser };
+
